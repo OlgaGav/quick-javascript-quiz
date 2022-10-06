7315346d0de9198a9f225218b11859b1f3e18743
Olga Gavrushenko <ogavby@gmail.com>
2022-10-06 12:00:10 -0700

    added list of questions and answers for the quiz

---
+++ assets/js/script.js
@@ -0,0 +1,70 @@
+
+//correct answer is an index of answer's array
+let questions = [
+	{
+		"question_test": "Arrays in JavaScript can be used to store:",
+		"answers": [
+			"Numbers and Strings",
+			"Other Arrays",
+			"Booleans",
+			"All of the Above"
+		],
+		"correct_answer": 3,
+	},
+		{
+            "question_test": "String values must be enclosed within ____ when being assigned to variables",
+            "answers": [
+                "Commas",
+                "Curly Brackets",
+                "Quotes",
+                "Parenthesis"
+		],
+		"correct_answer": 2, 
+	},
+		{
+            "question_test": "Commonly used data types DO NOT include:",
+            "answers": [
+                "Strings",
+                "Booleans",
+                "Alerts",
+                "Numbers"
+		],
+		"correct_answer": 2,
+	},
+		{
+            "question_test": "The condition of an if/else statement is enclosed with:",
+            "answers": [
+                "Quotes",
+                "Curly Brackets",
+                "Parenthesis",
+                "Square Brackets"
+		],
+		"correct_answer": 2,
+	},
+	{
+        "question_test": "As a developer, I want to be able to remove the last element of my array and I want to also be able to add a new element to the beginning of my array. Which two array methods should I use?",
+        "answers": [
+            "pop() and unshift()",
+            "push() and sort()",
+            "forEach() and pop()",
+            "concat() and shift()"
+		],
+		"correct_answer": 0,
+	},
+    {
+        "question_test": "How do we declare a conditional statement in JavaScript?",
+        "answers": [
+            "if...else",
+            "for loop",
+            "while loop",
+            "difference...between"
+    ],
+    "correct_answer": 0,
+}
+]
+
+
+
+
+
+
